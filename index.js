4eb293bd9a9e94119c584b35fe43407da75f20dc
fitsum belayneh <fitsu_alt@yahoo.com>
2020-01-03 15:39:28 -0500

    redirecting to about page

---
+++ index.js
@@ -0,0 +1,12 @@
+let aboutMe = document.querySelector(".btn1");
+// let home = document.querySelector(".fa-long-arrow-alt-left");
+
+aboutMe.addEventListener("click", () => {
+  console.log("clicked");
+  location.href = "about.html";
+});
+
+// home.addEventListener("click", () => {
+//   console.log("clicked");
+//   location.href = "index.html";
+// });
